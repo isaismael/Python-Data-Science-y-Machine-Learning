81d54a0a5aaa2e661c8b5730db49505e978a3948
Ismael Isa <ismael.isa02@gmail.com>
2025-12-10 16:31:48 -0300

    add limpieza de datos

--- 04-pandas-limpieza-de-datos/main.py
+++ 04-pandas-limpieza-de-datos/main.py
@@ -12,13 +12,48 @@ data = {
 df = pd.DataFrame(data)
 print("- - - Data Frame - - -")
 
-#info nos da datos importantes para nuestra limpieza, por ejemplo ver si hay valores nulos
+# info nos da datos importantes para nuestra limpieza, por ejemplo ver si hay valores nulos
 print("- - - Info - - -")
 print(df.info())
 
-#filtramos con el metodo isnull en bool para ver cuales estan vacios
+# filtramos con el metodo isnull en bool para ver cuales estan vacios
 print("- - - Null - - -")
 print(df.isnull())
 
+# sumamos los nulos de cada columna
+print("- - - Sum Null - - -")
+print(df.isnull().sum())
+
+# eliminamos los registros que contengan null
+print("- - - Delete Null - - -")
+df_deleted = df.dropna()
+print(df_deleted)
+
+# cambiamos los valores de los null
+print("- - - Change Null - - -")
+df_changed = df.fillna(0)
+print(df_changed)
+
+# aplicando cambios de valores null a columnas
+data_null = {
+    "Cantidad_vendida": 0,
+    "Precio": df["Precio"].mean()
+    # mean es el promedio, en este caso promedio de lo que haya en la columna en general
+}
+df_filled = df.fillna(data_null)
+print(df_filled)
+
+# - - -
+# correcion de tipo de datos
+print("- - - Change Type - - -")
+df_filled["Cantidad_vendida"] = df_filled["Cantidad_vendida"].astype(int)
+print(df_filled)
 
 
+# - - -
+# verificamos si tenemos duplicamos, los eliminamos
+# si no pasamos un parametro no hace ningun drop
+print("- - - Delete Duplicates - - -")
+# df_unique = df_filled.drop_duplicates()
+df_unique = df_filled.drop_duplicates(subset=["Id_producto"])
+print(df_unique)
